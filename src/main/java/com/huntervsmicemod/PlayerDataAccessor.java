e29180c9c47f5507254bd4a804fa2f6f3ab1ea51
MHN <1289955100@qq.com>
2025-07-26 21:18:21 +0800

    Revert "初始提交"
    This reverts commit a820befea67007cc162882d5bb2f5152b905bba2.

--- src/main/java/com/huntervsmicemod/PlayerDataAccessor.java
+++
@@ -1,8 +0,0 @@
-package com.huntervsmicemod;
-
-public interface PlayerDataAccessor {
-
-    PlayerData huntervsmicemod_template_1_20_1$getPlayerData();
-
-    void huntervsmicemod_template_1_20_1$setPlayerData(PlayerData data);
-}
